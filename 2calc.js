f380a327905acd47249baf89f4eddcd889d114d8
carlotata <avisojohn040@gmail.com>
2024-04-05 17:16:24 +0800

    git push -u origin main

--- 2calc.js
+++ 2calc.js
@@ -1,27 +1,32 @@
-function calculate(operator){
-
-    let num1 = document.getElementById('num1').value
-    let num2 = document.getElementById('num2').value
+function calculate(operator) {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
     var result;
 
-    if(num1 == '' || num2 == ''){
-        document.getElementById('result').innerText = 'ENTER NUMBER!'
-    }
-    else {
-        switch (operator){
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById('result').innerText = 'ENTER NUMBER!';
+    } else {
+        switch (operator) {
             case '+':
-                result = num1 + num2
-                break
+                result = num1 + num2;
+                break;
             case '-':
-                 result = num1 - num2
-                break
+                result = num1 - num2;
+                break;
             case '*':
-                 result = num1 * num2
-                 break
+                result = num1 * num2;
+                break;
             case '/':
-                 result = num1 / num2
-                 break
+                if (num2 === 0) {
+                    result = 'Cannot divide by zero';
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                result = 'Invalid operator';
         }
-        document.getElementById('result').innerText = result
+        document.getElementById('result').innerText = result;
     }
 }
+
